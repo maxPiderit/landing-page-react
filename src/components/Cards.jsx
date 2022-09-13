0ca73af5d058d159f3b3d8186002d8f56c9fc8eb
maxPiderit <maximiliano.piderit.a@gmail.com>
2022-09-13 20:13:12 +0000

    cambios Luis

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,16 +6,23 @@ import PropTypes from 'prop-types';
 const Cards = ({ cards }) => {
     return (
         <div className="row">
-            <div className="col">
-                <div className="card">
-                    <img src={cards.image} className="card-img-top" alt="" />
-                    <div className="card-body">
-                        <h5 className="card-title">{cards.title}</h5>
-                        <p className="card-text">{cards.description}</p>
-                        <a href={cards.buttonUrl} className="btn btn-primary">{cards.buttonLabel}</a>
-                    </div>
-                </div>
-            </div>
+            {
+                cards.map((card, index) => {
+                    return (
+                        <div className="col" key={index}>
+                            <div className="card">
+                                <img src={card.image} className="card-img-top" alt="" />
+                                <div className="card-body">
+                                    <h5 className="card-title">{card.title}</h5>
+                                    <p className="card-text">{card.title}</p>
+                                    <a href={card.buttonUrl} className="btn btn-primary">{card.buttonLabel}</a>
+                                </div>
+                            </div>
+                        </div>
+                    
+                )})
+            }
+
         </div>
     )
 
--- src/data/info.js
+++ src/data/info.js
@@ -28,9 +28,7 @@ export let footer = {
 }
 
 export let cards = [
-    {title:'Card title'},
-    {image:"https://edgewoodreit.com/wp-content/uploads/2018/01/500x325.png"},
-    {description: "Lorem ipsum dolor sit amet, consectuter adipisicing elit. Sapiente esse necessitatibus neque"},
-    {buttonUrl:""},
-    {buttonLabel:"Find Out More"}
+    {title: 'Card title', image: "https://edgewoodreit.com/wp-content/uploads/2018/01/500x325.png", buttonLabel: "Find Out More"},
+    {title: 'Card title', image: "https://edgewoodreit.com/wp-content/uploads/2018/01/500x325.png", buttonLabel: "Hola"},
+    {title: 'Card title', image: "https://edgewoodreit.com/wp-content/uploads/2018/01/500x325.png", buttonLabel: "Find Out More"}
 ]
